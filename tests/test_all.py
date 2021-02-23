00c4b6872e264521be99b8afb59f25f5de2e82ee
lupin3000 <steffenlorenz@hotmail.de>
2021-02-23 11:36:30 +0100

    add receive tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,60 +1,68 @@
 #!/usr/bin/env python
 
 import pytest
-from srfcat import SendTextSignal
-from srfcat import ReceiveTextSignal
+from srfcat.SendTextSignal import SendSignal
+from srfcat.ReceiveTextSignal import ReceiveSignal
 
 
 def test_send_frequency_true():
-    assert SendTextSignal.SendSignal.set_frequency(300000000) is None
+    assert SendSignal.set_frequency(300000000) is None
 
 
 def test_send_frequency_false():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        SendTextSignal.SendSignal.set_frequency(100000000)
+        SendSignal.set_frequency(100000000)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
 
 
 def test_send_baud_rate_true():
-    assert SendTextSignal.SendSignal.set_baud_rate(4000) is None
+    assert SendSignal.set_baud_rate(4000) is None
 
 
 def test_send_baud_rate_false():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        SendTextSignal.SendSignal.set_baud_rate(200)
+        SendSignal.set_baud_rate(200)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
 
 
 def test_send_repeats_true():
-    assert SendTextSignal.SendSignal.set_repeats(5) is None
+    assert SendSignal.set_repeats(5) is None
 
 
 def test_send_repeats_false():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        SendTextSignal.SendSignal.set_repeats(100)
+        SendSignal.set_repeats(100)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
 
 
 def test_receive_frequency_true():
-    assert ReceiveTextSignal.ReceiveSignal.set_frequency(300000000) is None
+    assert ReceiveSignal.set_frequency(300000000) is None
 
 
 def test_receive_frequency_false():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        ReceiveTextSignal.ReceiveSignal.set_frequency(100000000)
+        ReceiveSignal.set_frequency(100000000)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
 
 
 def test_receive_baud_rate_true():
-    assert ReceiveTextSignal.ReceiveSignal.set_baud_rate(4000) is None
+    assert ReceiveSignal.set_baud_rate(4000) is None
 
 
 def test_receive_baud_rate_false():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        ReceiveTextSignal.ReceiveSignal.set_baud_rate(200)
+        ReceiveSignal.set_baud_rate(200)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
+
+
+def test_receive_lowball_true():
+    assert ReceiveSignal.set_lowball(True) is None
+
+
+def test_receive_max_power_true():
+    assert ReceiveSignal.set_max_power(True) is None
